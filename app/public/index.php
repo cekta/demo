92852c140a05bf772238e0811124c1c9c11a9dff
Evgeniy Kuvshinov <smpl@itmywork.com>
2019-09-30 01:55:04 +0500

    Внедрение зависимостей cekta/di

--- app/public/index.php
+++ app/public/index.php
@@ -1,23 +1,14 @@
 <?php
 declare(strict_types=1);
 
-use App\ExampleContainer;
-use App\MyMatcher;
+use App\MyContainer;
 use Cekta\HTTP\Server\Application;
 use Cekta\HTTP\Server\SapiEmitter;
-use Cekta\Routing\Nikic\ProviderHandler;
-use Cekta\Routing\Nikic\ProviderMiddleware;
 use Zend\Diactoros\ServerRequestFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$container = new ExampleContainer();
-$providerHandler = new ProviderHandler($container);
-$providerMiddleware = new ProviderMiddleware($container);
-$matcher = new MyMatcher($providerHandler, $providerMiddleware);
-$request = ServerRequestFactory::fromGlobals();
-$application = new Application($matcher);
-$response = $application->handle($request);
-
-$emiter = new SapiEmitter();
-$emiter->emit($response);
+$container = new MyContainer();
+$container->get(SapiEmitter::class)->emit(
+    $container->get(Application::class)->handle(ServerRequestFactory::fromGlobals())
+);

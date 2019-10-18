4a6a4cb1d57394eb917db576efb1ad1cf9a2277d
Evgeniy Kuvshinov <smpl@itmywork.com>
2019-10-18 15:30:09 +0500

    env с трансформацией значений

--- app/src/Container.php
+++ app/src/Container.php
@@ -13,7 +13,28 @@ class Container extends \Cekta\DI\Container
     {
         $dotenv = Dotenv::create(__DIR__ . '/../');
         $dotenv->load();
-        $providers[] = new KeyValue(getenv());
+        $providers[] = new KeyValue(array_map(function ($value) {
+            if (is_string($value)) {
+                switch (strtolower($value)) {
+                    case 'true':
+                    case '(true)':
+                        return true;
+                    case 'false':
+                    case '(false)':
+                        return false;
+                    case 'empty':
+                    case '(empty)':
+                        return '';
+                    case 'null':
+                    case '(null)':
+                        return null;
+                }
+                if (preg_match('/\A([\'"])(.*)\1\z/', $value, $matches)) {
+                    return $matches[2];
+                }
+            }
+            return $value;
+        },$_SERVER + $_ENV + getenv()));
         $providers[] = KeyValue::stringToAlias(require __DIR__ . '/../implementation.php');
         $providers[] = KeyValue::closureToService(require __DIR__ . '/../service.php');
         $providers[] = new Autowiring();
